6b223d7b1c6ab2fb0c16b9c030b57b72144eceb2
Stephen Rayner <srayner@neworbit.co.uk>
2019-06-09 14:06:19 +0100

    refactor reconigzer context into modle builder

--- src/api/inkRec.ts
+++ src/api/inkRec.ts
@@ -1,6 +1,7 @@
 import * as MyScript from "myscript";
 import { Strokes } from "../models/Strokes";
 import { jixxModelBuilderDto } from "../models/jixxModelBuilder";
+import { RecognizerContext } from "../models/recognizerContext";
 
 const convertBlobToBase64 = (blob: any) =>
     new Promise((resolve, reject) => {
@@ -26,7 +27,7 @@ export const inkRec = async (strokes: Strokes[]) => {
                     })
                     .catch(error => {
                         // eslint-disable-next-line no-console
-                        console.log("Unable to convert to base 64");
+                        console.error("Unable to convert to base 64");
                     });
             } else {
                 interpretatedTextContent = JSON.stringify(
@@ -36,7 +37,7 @@ export const inkRec = async (strokes: Strokes[]) => {
         });
     };
 
-    const onClickRecognizeAsync = () => {
+    const onClickRecognizeAsync = (): Promise<string> => {
         return new Promise((resolve, reject) => {
             const iinkRecognizer = MyScript.DefaultBehaviors.recognizerList.find(
                 (x: any) => {
@@ -56,23 +57,7 @@ export const inkRec = async (strokes: Strokes[]) => {
             );
 
             // Creating a recognizer context with the configuration attached
-            const recognizerContext = MyScript.RecognizerContext.createEmptyRecognizerContext(
-                {
-                    configuration: MyScript.DefaultConfiguration
-                }
-            );
-
-            //const mimeType = 'application/vnd.myscript.jiix';
-            const requestedMimeType = ["text/plain", "image/png"];
-            recognizerContext.editor.configuration.recognitionParams.protocol =
-                "REST";
-            recognizerContext.editor.configuration.recognitionParams.v4.text.mimeTypes = requestedMimeType;
-            recognizerContext.editor.configuration.recognitionParams.server = {
-                scheme: "https",
-                host: "webdemoapi.myscript.com",
-                applicationKey: "8bb3b361-d81d-4aef-8a32-56fc3e0cd1dc",
-                hmacKey: "4a86f94d-ad5a-4e24-88d8-623922fa890a"
-            };
+            const recognizerContext = RecognizerContext(MyScript);
 
             // Assigning a theme to the document
             recognizerContext.editor.theme = MyScript.DefaultTheme;
